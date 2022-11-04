a436de5a3cebe32108795cb1acad945ad7803a55
Saikrishna <84697463+saikrishna5555@users.noreply.github.com>
2022-11-04 23:39:21 +0530

    import java.lang.Math; import java.util.Scanner; class Sai{     public static void main (String[] args){         Scanner sc=new Scanner (System.in);         int a;         a=sc.nextInt();         double r=(3.14)*a*a;         System.out.printf("%.2f",r);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+import java.util.Scanner;
+class Sai{
+    public static void main (String[] args){
+        Scanner sc=new Scanner (System.in);
+        int a;
+        a=sc.nextInt();
+        double r=(3.14)*a*a;
+        System.out.printf("%.2f",r);
+    }
+}
