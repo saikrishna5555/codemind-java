6cd58737c9813b4a5529f870474d426ee8709c1d
Saikrishna <84697463+saikrishna5555@users.noreply.github.com>
2022-11-05 12:11:24 +0530

    import java.util.Scanner; class Sai1{     public static void main (String[] args){         Scanner sc=new Scanner (System.in);         int a,b;         a=sc.nextInt();         b=sc.nextInt();         int r=a*b;         System.out.println(r);
        }
    }

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Sai1{
+    public static void main (String[] args){
+        Scanner sc=new Scanner (System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        int r=a*b;
+        System.out.println(r);
+
+    }
+}
