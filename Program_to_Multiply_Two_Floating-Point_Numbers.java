a93b541585305f06cbc2a3c1dcd15df89bd06ecb
Saikrishna <84697463+saikrishna5555@users.noreply.github.com>
2022-11-06 11:56:18 +0530

    import java.util.Scanner; class ASfa{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         float a = sc.nextFloat();         float b = sc.nextFloat();         float r = a*b;         System.out.printf("%.2f",r);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class ASfa{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        float r = a*b;
+        System.out.printf("%.2f",r);
+    }
+}
