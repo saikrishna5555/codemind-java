360323173a4a9156cb8ff9e56b1e8376ad70c7d5
Saikrishna <84697463+saikrishna5555@users.noreply.github.com>
2022-11-06 12:13:31 +0530

    import java.util.Scanner; class Sai{     public static void main(String[] args){         Scanner sc= new Scanner(System.in);         char s= sc.next().charAt(0);         int a=s;         System.out.println(a);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Sai{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        char s= sc.next().charAt(0);
+        int a=s;
+        System.out.println(a);
+    }
+}
