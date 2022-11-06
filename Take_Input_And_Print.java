3e6e9b3bbe78827ff7b954b4b4b1fa0eb0dcdb9c
Saikrishna <84697463+saikrishna5555@users.noreply.github.com>
2022-11-06 12:00:45 +0530

    import java.util.Scanner; class ASfa{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         String a = sc.nextLine();         System.out.println(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class ASfa{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.println(a);
+    }
+}
