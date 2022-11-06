c552276e4ec2563eb27e374904f9f6bcf6fee35e
Saikrishna <84697463+saikrishna5555@users.noreply.github.com>
2022-11-06 17:49:03 +0530

    import java.util.Scanner; class Sai {     public static void main(String[] args)     {         Scanner sc= new Scanner(System.in);         int a=sc.nextInt();         int r=(a*(a+1))/2;         System.out.println(r);
        }
    }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Sai
+{
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+        int r=(a*(a+1))/2;
+        System.out.println(r);
+    
+    }
+}
