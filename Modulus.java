25de1ec5e5479ff9492724d9831cda105f3539df
Saikrishna <84697463+saikrishna5555@users.noreply.github.com>
2022-11-06 11:58:48 +0530

    import java.util.Scanner; class ASfa{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int b = sc.nextInt();         int r = a%b;         System.out.println(r);     } }

---
+++ Modulus.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class ASfa{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int r = a%b;
+        System.out.println(r);
+    }
+}
