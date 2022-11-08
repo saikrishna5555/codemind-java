ba65ffed76d4787e62a8aa66365ba9eb2e57efd1
Saikrishna <84697463+saikrishna5555@users.noreply.github.com>
2022-11-08 21:20:18 +0530

    import java.util.*; class Sdjl {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int s=0;         for (int i=1;i!=a;i++)         {             if (a%i==0)             {                 s+=i;             }         }         if (s>a){             System.out.println("True");         }         else{             System.out.println("False");         }     } }

---
+++ Abundant_Numbers.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Sdjl
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int s=0;
+        for (int i=1;i!=a;i++)
+        {
+            if (a%i==0)
+            {
+                s+=i;
+            }
+        }
+        if (s>a){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+    }
+}
