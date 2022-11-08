a71c8f03fb8e2dac5872bc9329f223d5ee70571d
Saikrishna <84697463+saikrishna5555@users.noreply.github.com>
2022-11-08 21:53:00 +0530

    import java.util.*; class Sdjl {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int t=sc.nextInt();         for (int i=0;i!=t;i++)         {             int a,b;             a=sc.nextInt();             b=sc.nextInt();             System.out.println(a+b);         }     } }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Sdjl
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for (int i=0;i!=t;i++)
+        {
+            int a,b;
+            a=sc.nextInt();
+            b=sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
